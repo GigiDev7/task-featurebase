ea71783baf2c138894e3f0b4b1d323b46881df5b
GigiDev7 <gigivadachkoria@gmail.com>
2025-04-21 22:25:24 +0400

    Fix: fixed modal close button

--- app/components/posts/AddPostModal.tsx
+++ app/components/posts/AddPostModal.tsx
@@ -95,7 +95,19 @@ export default function AddPostModal({ closeModal, isOpen }: Props) {
           onClick={closeModal}
           className="absolute top-4 right-4 text-gray-400 hover:text-gray-600 cursor-pointer"
         >
-          ✕
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+            className="w-4.5 h-4.5 cursor-pointer secondary-svg main-transition"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
         </button>
 
         <div className="flex items-center gap-2 mb-4">
